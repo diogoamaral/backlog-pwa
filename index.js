b2d41d900e4c7e2e060d652eab7cae1b50e0c196
Diogo Cezar Amaral <diogo.amaral@aguiasistemas.com.br>
2016-09-20 09:18:09 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var path    = require('path');
+
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(path.join(__dirname, '/public'),
+                       {'index': ['index.html']}))
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
